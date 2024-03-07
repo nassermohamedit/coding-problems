78cc3a3457fe871ed4d4315d2e61444e9c703cf5
Nasser Mohamed <nassermohamedit@gmail.com>
2024-03-07 14:05:34 +0100

    I met a javascript guy who knows a data structure other than maps, I m literally shaking

---
+++ utils/LinkedList.js
@@ -0,0 +1,31 @@
+
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null
+    }
+
+    push(e) {
+        let node = Node(data);
+        if (this.head) node.next = this.head;
+        this.head = node;
+    }
+
+    isEmpty() {
+        return this.head == null;
+    }
+
+    pop() {
+        if (!this.head) throw new Error("List is empty");
+        let data = this.head.data;
+        this.head = this.head.next;
+        return data;
+    }
+}
