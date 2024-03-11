8bf613b53755a917f00e2ed63d9f3e3c815f07c8
Nasser Mohamed <nassermohamedit@gmail.com>
2024-03-11 00:37:36 +0000

    efficient Scanner

---
+++ utils/Scanner.java
@@ -0,0 +1,33 @@
+import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+
+class Scanner {
+
+    private final BufferedReader br;
+    private StringTokenizer st;
+
+
+    public Scanner(InputStream in) {
+        this.br = new BufferedReader(new InputStreamReader(in));
+    }
+
+    public String next() {
+        while (st == null || !st.hasMoreTokens() ) {
+            try {
+                st = new StringTokenizer(this.br.readLine());
+            } catch (IOException e) {
+                throw new RuntimeException();
+            }
+        }
+        return st.nextToken();
+    }
+
+    public int nextInt() {
+        return Integer.parseInt(next());
+    }
+}
+
