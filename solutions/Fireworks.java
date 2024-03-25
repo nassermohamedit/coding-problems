f2d23d73cc0b651b0054cc695a1a26946d35729f
Nasser Mohamed <nassermohamedit@gmail.com>
2024-03-25 20:53:05 +0000

    math

---
+++ solutions/Fireworks.java
@@ -0,0 +1,54 @@
+/**
+ * tags: math
+ * statement: https://codeforces.com/problemset/problem/1945/B
+ */
+
+import java.io.*;
+import java.util.*;
+
+public class Fireworks {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            long a = sc.nextLong();
+            long b = sc.nextLong();
+            long m = sc.nextLong();
+            out.write((m/a + m/b + 2) + "\n");
+        }
+        out.flush();
+    }
+}
+
+
+class Scanner {
+
+    private final BufferedReader br;
+    private StringTokenizer st;
+
+    public Scanner(InputStream in) {
+        this.br = new BufferedReader(new InputStreamReader(in));
+    }
+
+    public String next() {
+        while (st == null || !st.hasMoreTokens() ) {
+            try {
+                st = new StringTokenizer(this.br.readLine());
+            } catch (IOException e) {
+                throw new RuntimeException();
+            }
+        }
+        return st.nextToken();
+    }
+
+    public int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+    public long nextLong() {
+        return Long.parseLong(next());
+    }
+}
+
