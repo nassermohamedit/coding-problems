dea4a4b1b50fadc59f4a285d247491b5c3442093
Nasser Mohamed <nassermohamedit@gmail.com>
2024-03-26 00:27:06 +0000

    math

---
+++ solutions/DestroyingBridges.java
@@ -0,0 +1,55 @@
+/**
+ * tags: math
+ * statement: https://codeforces.com/problemset/problem/1944/A
+ */
+
+import java.io.*;
+import java.util.*;
+
+
+public class DestroyingBridges {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            if (k < n - 1) out.write(n + "\n");
+            else out.write("1\n");
+        }
+        out.flush();
+    }
+}
+
+
+class Scanner {
+
+    private final BufferedReader br;
+    private StringTokenizer st;
+
+    public Scanner(InputStream in) {
+        this.br = new BufferedReader(new InputStreamReader(in));
+    }
+
+    public String next() {
+        while (st == null || !st.hasMoreTokens() ) {
+            try {
+                st = new StringTokenizer(this.br.readLine());
+            } catch (IOException e) {
+                throw new RuntimeException();
+            }
+        }
+        return st.nextToken();
+    }
+
+    public int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+    public long nextLong() {
+        return Long.parseLong(next());
+    }
+}
+
