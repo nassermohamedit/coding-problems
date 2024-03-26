0dc2c9b0af4799fa9cbd46b11dfceb7e3fc70eae
Nasser Mohamed <nassermohamedit@gmail.com>
2024-03-26 14:19:48 +0000

    math

---
+++ one-month-prep-kit/PlusMinus.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void plusMinus(List<Integer> arr) {
+        int n = arr.size();
+        int p = 0;
+        int z = 0;
+        int ne = 0;
+        for (int i: arr) {
+            if (i > 0) ++p;
+            else if (i == 0) ++z;
+            else ++ne;
+        }
+        System.out.println((double)p/n);
+        System.out.println((double)ne/n);
+        System.out.println((double)z/n);
+    }
+
+}
+
+public class PlusMinus {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        Result.plusMinus(arr);
+
+        bufferedReader.close();
+    }
+}
+
