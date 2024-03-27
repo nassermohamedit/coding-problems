9d5dad4f94897e1f6c865be0244cd1290e0d4897
Nasser Mohamed <nassermohamedit@gmail.com>
2024-03-27 02:49:20 +0000

    data stuctures

---
+++ one-month-prep-kit/week1/SparseArrays.java
@@ -0,0 +1,74 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * statement: https://www.hackerrank.com/challenges/one-month-preparation-kit-sparse-arrays
+     */
+
+    public static List<Integer> matchingStrings(List<String> strings, List<String> queries) {
+        List<Integer> ans = new ArrayList<>();
+        Map<String, Integer> counts = new HashMap<>();
+        for (String s: strings) {
+            if (!counts.containsKey(s)) counts.put(s, 1);
+            else counts.put(s, counts.get(s) + 1);
+        }
+        for (String s: queries) {
+            ans.add((counts.containsKey(s))? counts.get(s): 0);
+        }
+        return ans;
+    }
+
+}
+
+public class SparseArrays {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int stringsCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<String> strings = IntStream.range(0, stringsCount).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        })
+            .collect(toList());
+
+        int queriesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<String> queries = IntStream.range(0, queriesCount).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        })
+            .collect(toList());
+
+        List<Integer> res = Result.matchingStrings(strings, queries);
+
+        bufferedWriter.write(
+            res.stream()
+                .map(Object::toString)
+                .collect(joining("\n"))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
