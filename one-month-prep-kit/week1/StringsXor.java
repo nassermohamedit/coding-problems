93ad8149fe8049e0dbf5f7d6aec7e6e97fbbd13a
Nasser Mohamed <nassermohamedit@gmail.com>
2024-03-27 07:07:02 +0000

    strings

---
+++ one-month-prep-kit/week1/StringsXor.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class StringsXor {
+
+    /*
+     * statement: https://www.hackerrank.com/challenges/one-month-preparation-kit-strings-xor/
+    */
+
+    public static String stringsXOR(String s, String t) {
+        String res = new String("");
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == t.charAt(i))
+                res += "0";
+            else
+                res += "1";
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+        String s, t;
+        Scanner in = new Scanner(System.in);
+        s = in.nextLine();
+        t = in.nextLine();
+        System.out.println(stringsXOR(s, t));
+
+    }
+
+}
+
+
+
+
