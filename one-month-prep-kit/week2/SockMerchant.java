d10b6de6b126c3e3840b76f7ddb2edb554e92896
Nasser Mohamed <nassermohamedit@gmail.com>
2024-04-01 12:58:03 +0000

    ds

---
+++ one-month-prep-kit/week2/SockMerchant.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * statement: https://www.hackerrank.com/challenges/one-month-preparation-kit-sock-merchant
+     */
+
+    public static int sockMerchant(int n, List<Integer> ar) {
+        int[] socks = new int[105];
+        for (int s: ar) {
+            ++socks[s];
+        }
+        int ans = 0;
+        for (int k: socks) {
+            ans += k / 2;
+        }
+        return ans;
+    }
+
+}
+
+public class SockMerchant {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.sockMerchant(n, ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
