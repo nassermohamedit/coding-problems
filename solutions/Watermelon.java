37e3a41ace1b64a0f5ad5b6a921ee9a09b75f24a
Nasser Mohamed <nassermohamedit@gmail.com>
2024-04-04 23:49:58 +0000

    math

---
+++ solutions/Watermelon.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * statement: https://codeforces.com/problemset/problem/4/A
+ */
+
+
+public class Watermelon {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+        int w = sc.nextInt();
+        if (w%2 == 0 && w/2 > 1) out.write("Yes");
+        else out.write("No");
+        out.flush();
+    }
+}
+
+
+class Scanner {
+
+    private final BufferedReader br;
+    private StringTokenizer st;
+
+    public Scanner(InputStream in) {
+        this.br = new BufferedReader(new InputStreamReader(in));
+    }
+
+    public String next() {
+        while (st == null || !st.hasMoreTokens() ) {
+            try {
+                st = new StringTokenizer(this.br.readLine());
+            } catch (IOException e) {
+                throw new RuntimeException();
+            }
+        }
+        return st.nextToken();
+    }
+
+    public int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+    public long nextLong() {
+        return Long.parseLong(next());
+    }
+}
+
