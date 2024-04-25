a286db1f66bd0b739174428fea10b29bb0659231
Nasser Mohamed <nassermohamedit@gmail.com>
2024-04-25 02:55:23 +0100

    modular exponentiation for largs powers

---
+++ math-problems/exponentiation2.rs
@@ -0,0 +1,53 @@
+use std::arch::x86_64::_mm_sha1nexte_epu32;
+#[allow(unused_imports)]
+use std::cmp::{min, max};
+use std::io::{BufWriter, stdin, stdout, Write};
+
+const MOD: u32 = 1000000007;
+
+
+fn main() {
+    let mut sc = Scanner::default();
+    let mut out = BufWriter::new(stdout());
+    let n: usize = sc.next();
+    for _i in 0..n {
+        let mut a: u64 = sc.next();
+        let mut b: u64 = sc.next();
+        let mut c: u64 = sc.next();
+        let mut k: u64 = 1;
+        while c > 0 {
+            if c & 1 != 0 {
+                k = k * b % (MOD as u64 - 1);
+            }
+            b = b * b % (MOD as u64 - 1);
+            c >>= 1;
+        }
+        let mut res: u64 = 1;
+        while k > 0 {
+            if k & 1 != 0 {
+                res = res * a % (MOD as u64);
+            }
+            a = a * a % (MOD as u64);
+            k >>= 1;
+        }
+        writeln!(out, "{}", res);
+    }
+}
+
+
+#[derive(Default)]
+struct Scanner {
+    buffer: Vec<String>
+}
+impl Scanner {
+    fn next<T: std::str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buffer.pop() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            let mut input = String::new();
+            stdin().read_line(&mut input).expect("Failed read");
+            self.buffer = input.split_whitespace().rev().map(String::from).collect();
+        }
+    }
+}
