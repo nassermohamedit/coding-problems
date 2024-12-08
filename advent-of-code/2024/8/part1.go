386b1ab0511a315c91a22d6a19475a1facc15af3
Nasser Mohamed <nassermohamedit@gmail.com>
2024-12-08 16:57:56 +0100

    AoC 24 - day 8

---
+++ advent-of-code/2024/8/part1.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"bufio"
+)
+
+var fname = "in"
+var in  *bufio.Scanner
+
+func init() {
+	f, _ := os.Open(fname)
+	in = bufio.NewScanner(f)
+}
+
+func main() {
+	in.Scan()
+	var line = in.Text()
+	var n = len(line)
+	var k = 0
+	var mm = make(map[rune][]int)
+	updateMap(0, mm, line)
+	for in.Scan() {
+		k++
+		line = in.Text()
+		updateMap(k*n, mm, line)
+	}
+	var positions = make(map[int]bool)
+	k++
+	for _, locations := range mm {
+		for i := 0; i < len(locations); i++ {
+			for j := i + 1; j < len(locations); j++ {
+				p, q := antinodes(locations[i], locations[j], n, k)
+				if p != -1 {
+					positions[p] = true
+				}
+				if q != -1 {
+					positions[q] = true
+				}
+			}
+		}
+	}
+	fmt.Println(len(positions))
+}
+
+func updateMap(k int, mm map[rune][]int, line string) {
+	for i, c := range line {
+		if c != '.' {
+			arr, exist := mm[c]
+			if !exist {
+				mm[c] = make([]int, 0, 1000)
+			}
+			mm[c] = append(arr, k + i)
+		}
+	}	
+}
+
+func antinodes(a int, b int, n int, k int) (int, int) {
+	x1, y1 := a%n, a/n
+	x2, y2 := b%n, b/n
+	dx := x2 - x1
+	dy := y2 - y1
+	p, q := -1, -1
+	xx, yy := x1 - dx, y1 - dy
+	if within_map(xx, yy, n, k) {
+		p = yy*n + xx
+	}
+	xx = x2 + dx
+	yy = y2 + dy
+	if within_map(xx, yy, n, k) {
+		q = yy*n + xx
+	}
+	return p, q
+}
+
+func within_map(x int, y int, n int, k int) bool {
+	return x >= 0 && x < n && y >= 0 && y < k
+}
